b2df1b8630ef090515dd79a3a0f080ac3969c371
pilzify <pneumann3@gmx.de>
2023-12-30 22:17:38 +0100

    test 01

--- src/main/java/net/pilzify/wombieworld/WombieWorld.java
+++ src/main/java/net/pilzify/wombieworld/WombieWorld.java
@@ -7,6 +7,7 @@ public final class WombieWorld extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("hello world");
 
     }
 
