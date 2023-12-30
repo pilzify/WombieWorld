03cbcec605660733cd636d80fe22c73f9d161b63
pilzify <pneumann3@gmx.de>
2023-12-30 21:52:43 +0100

    initial commit

---
+++ src/main/java/net/pilzify/wombieworld/WombieWorld.java
@@ -0,0 +1,18 @@
+package net.pilzify.wombieworld;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WombieWorld extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+
+    }
+}
