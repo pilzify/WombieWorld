1aada7fea559ffd9b02e200989366a2016071ea6
pilzify <pneumann3@gmx.de>
2024-01-02 01:08:49 +0100

    .1.2 modified spawner, added util, implemented default zombie

--- src/main/java/net/pilzify/wombieworld/entities/Spawner.java
+++ src/main/java/net/pilzify/wombieworld/entities/Spawner.java
@@ -1,7 +1,60 @@
 package net.pilzify.wombieworld.entities;
 
+import net.pilzify.wombieworld.WombieWorld;
+import net.pilzify.wombieworld.utils.Millis;
+import org.bukkit.Location;
+import org.bukkit.scheduler.BukkitRunnable;
+
 public class Spawner {
-    public void spawnEntity(int x, int y, int z, Spawnable entity){
+    private final WombieWorld plugin = WombieWorld.getInstance();
+    private boolean active = true;
+    private int count = 0;
+    public void spawnEntities(Location location, Spawnable entity, int amount, long delay){
         //maybe something like this later on;
+        if (amount < 0){
+            throw new IllegalArgumentException("amount must be a positive integer");
+        }
+        new BukkitRunnable(){
+            @Override
+            public void run(){
+                if (count >= amount) {
+                    this.cancel();
+                    return;
+                }
+                entity.spawn(location);
+                count++;
+            }
+        }.runTaskTimer(plugin, 0, delay);
     }
+
+    public void spawnEntityPeriodically(Location location, Spawnable entity, long delay){
+        new BukkitRunnable(){
+            @Override
+            public void run(){
+                if (!active) {
+                    this.cancel();
+                    return;
+                }
+                entity.spawn(location);
+            }
+        }.runTaskTimer(plugin, 0, delay);
+    }
+
+    public void spawnEntityLater(Location location, Spawnable entity, long delay){
+        new BukkitRunnable(){
+            @Override
+            public void run(){
+                if (!active) {
+                    this.cancel();
+                    return;
+                }
+                entity.spawn(location);
+            }
+        }.runTaskLater(plugin, delay);
+    }
+
+    public void disable(){
+        this.active = false;
+    }
+
 }
